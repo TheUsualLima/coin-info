86ad2d9071ce48da6b339fcea54a5b92f5d31307
Navin Jason Hewadewa <nhewadewa@nn4m.com>
2023-08-27 22:53:40 +0100

    Divider

--- app/src/main/java/com/example/coininfo/ui/home/Home.kt
+++ app/src/main/java/com/example/coininfo/ui/home/Home.kt
@@ -4,18 +4,20 @@ package com.example.coininfo.ui.home
 
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.coininfo.R
 import com.example.coininfo.ui.composables.BaseScaffold
 import com.example.coininfo.ui.home.composables.CoinItem
@@ -33,16 +35,20 @@ fun Home(
                 Text(text = "There was an error")
             } else {
                 LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                    items(
+                    itemsIndexed(
                         items = state.value.coinData,
-                        key = {
-                            it.id
+                        key = { _, coin ->
+                            coin.id
                         }
-                    ) { coin ->
+                    ) { index, coin ->
                         CoinItem(
                             coin = coin,
-                            modifier = Modifier.animateItemPlacement(animationSpec = tween(500))
+                            modifier = Modifier
+                                .animateItemPlacement(animationSpec = tween(500))
+                                .padding(vertical = 8.dp)
                         )
+                        if (index < state.value.coinData.lastIndex)
+                            Divider(modifier = Modifier.padding(horizontal = 24.dp))
                     }
                 }
             }
